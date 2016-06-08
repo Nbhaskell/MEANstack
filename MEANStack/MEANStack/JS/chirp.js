3fd901e2b079b1e95e57de80db7fb84883cd318d
Nbhaskell <nbhaskell@gmail.com>
2016-06-08 12:36:27 -0700

    angular built out half way, starting to build out back end

---
+++ MEANStack/MEANStack/JS/chirp.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module('chirp', []);
+
+app.controller('mainController', function($scope){
+    $scope.posts = [];
+    $scope.newPost = {create_by: '', text: '', create_at: ''};
+
+    $scope.post = function(){
+        $scope.newPost.created_at = Date.now();
+        $scope.posts.push($scope.newPost);
+        $scope.newPost = {created_by: '', text: '', created_at: ''};
+    };
+});
+
+app.controller('authController', function ($scope) {
+    $scope.user = {username: '', password: ''};
+    $scope.error_message = '';
+
+    $scope.login = function () {
+        //Placeholder until authentication is implimented
+        $scope.error_message = 'login request for ' + $scope.user.username;
+    };
+
+    $scope.register = function () {
+        //Placeholder until authentication is implimented
+        $scope.error_message = 'registration request for ' + $scope.user.username;
+    };
+});
